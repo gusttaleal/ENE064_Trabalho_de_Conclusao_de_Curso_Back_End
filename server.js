3b1a245649dc216ba215f4f998f68506b1d82527
gusttaleal <lses.gustavo@gmail.com>
2022-06-20 12:07:54 -0300

    fix: server

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+var http = require("http");
 
 const deviceRouter = require("./src/router/DeviceRouter.js");
 const dataRouter = require("./src/router/DataRouter.js");
 const authRouter = require("./src/router/AuthRouter.js");
 
 const app = express();
+var server = http.createServer(app);
 
 app.use(
   cors({
@@ -18,7 +20,16 @@ app.use("/device", deviceRouter);
 app.use("/data", dataRouter);
 app.use("/auth", authRouter);
 
-const PORT = 5000;
-const server = app.listen(PORT, () => console.log(`http://localhost:${PORT}/`));
+const PORT = process.env.PORT;
+const hostname = process.env.HOSTNAME;
+
+server.listen(PORT, hostname);
+server.on("listening", () => {
+  console.log(
+    "Express server started on port %s at %s",
+    server.address().port,
+    server.address().address
+  );
+});
 
 module.exports = server;
